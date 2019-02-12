a958c17f56583691398f8e9410d9fe178f08480c
github username <itsDario1@gmail.com>
2019-02-12 00:27:37 -0500

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,9 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.keys.each do |kid|
+    puts "Happy Birthday #{kid}! You are now #{birthday_kids[kid]} years old!"
+  end
 end
 
 
